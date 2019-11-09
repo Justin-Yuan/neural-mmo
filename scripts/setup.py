cde2c666225d1382abb33243735f60e37113a267
Justin Yuan <justin.zcyuan@gmail.com>
2019-11-09 12:49:18 -0500

    fix dependencies, get mmo running

--- scripts/setup.py
+++ scripts/setup.py
@@ -4,7 +4,16 @@ import os
 #Install python requirements
 print('Installing requirements')
 os.system('pip install -r scripts/requirements.txt')
+
+# my fixes
 os.system('pip install imageio')
+os.system('pip install pygame')
+os.system('pip install psutil')
+# if it's mac, link to another file, run with `open client`
+import sys 
+if "darwin" in sys.platform:    
+    os.system('ln -fs $(pwd)/forge/embyr/UnityClient/neural-mmo-mac.app client')
+
 
 #Download the Unity client
 print('Downloading Embyr client')
